a8399317b7b5e55e1d96a9ceccd7c8798f0ea26f
jondominguez94 <jon.dominguez94@yahoo.com>
2018-12-26 15:55:45 -0800

    Setup mongoose

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
+const mongoose = require("mongoose");
 const express = require("express");
+
 const app = express();
+const db = require('./config/keys').mongoURI;
+
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("Hey World"));
 
