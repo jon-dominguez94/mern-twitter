c01c2fb17a88372fd007d6a1b6a0691907599f15
jondominguez94 <jon.dominguez94@yahoo.com>
2018-12-26 17:06:47 -0800

    Add user login route

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
+
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
 const User = require("../../models/User");
 
 router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
@@ -27,6 +30,27 @@ router.post('/register', (req, res) => {
           });
         });
       }
+    });
+});
+
+router.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({ email })
+    .then(user => {
+      if (!user) {
+        return res.status(401).json({ email: 'This user does not exist' });
+      }
+
+      bcrypt.compare(password, user.password)
+        .then(isMatch => {
+          if(isMatch){
+            res.json({ msg: 'Success' });
+          } else {
+            return res.status(400).json({ password: 'Incorrect password' });
+          }
+        });
     });
 });
 
