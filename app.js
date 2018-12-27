49e7c463b73fb58125b0ccfc027dc40396e819b8
jondominguez94 <jon.dominguez94@yahoo.com>
2018-12-27 13:24:24 -0800

    Setup project for heroku

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ const passport = require("passport");
 const express = require("express");
 const app = express();
 
+const path = require('path');
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('frontend/build'));
+  app.get('/', (req,res) => {
+    res.sendFile(path/resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+
 const mongoose = require("mongoose");
 const db = require('./config/keys').mongoURI;
 
